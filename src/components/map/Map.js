85c45854b9e1fee8f6883b0c97ed7d761fb745f7
Timo <ttimoww@gmail.com>
2019-04-08 14:32:08 +0200

    Added working map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
     state = {  }
+    static defaultProps = {
+        mapCenter : {
+            lat: 52.156113,
+            lng: 5.387827
+        },
+        zoom: 15
+    }
+
     render() { 
         return ( 
             <div className="map">
-                <p>Map Component</p>
+                <GoogleMapReact
+                    bootstrapURLKeys = {{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+                    defaultCenter = {this.props.mapCenter}
+                    defaultZoom = {this.props.zoom}
+                />
             </div>
          );
     }
